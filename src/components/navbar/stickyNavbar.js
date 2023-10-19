84f67a7e46ffce98d779085b08f9de4af254f29a
Davut Burak Dündar <davutburakduundar@gmail.com>
2023-10-19 15:14:28 +0000

    updated stickyNavbar

--- src/components/navbar/stickyNavbar.js
+++ src/components/navbar/stickyNavbar.js
@@ -1,26 +1,37 @@
 import Link from "next/link";
 import { defaultNavigation } from "./Navigation";
+import { Info } from "react-feather";
 
 export default function StickyNavbar() {
   return (
     <nav
-      className="sticky -top-px bg-white text-sm font-medium text-black ring-1 ring-gray-900 ring-opacity-5 border-t shadow-sm shadow-gray-100 py-2 -mt-px"
+      className="sticky w-full -top-px -mt-px bg-white text-black text-sm font-medium border-y border-gray-200 shadow-sm shadow-gray-200 py-2"
       aria-label="Jump links"
     >
-      <div className="max-w-7xl snap-x w-full flex items-center overflow-x-auto scrollbar-x px-4 py-2 mx-auto">
-        {defaultNavigation.map((link) => (
-          <div
-            className="snap-center shrink-0 pr-5"
-            key={link.id}
-          >
-            <Link
-              className="inline-flex items-center gap-x-2 hover:text-gray-500"
-              href={link.url}
+      <div className="flex basis-full items-center mx-auto max-w-7xl">
+        <div className="basis-1/2 snap-x flex items-center overflow-x-auto scrollbar-x px-4 py-2 mx-auto">
+          {defaultNavigation.map((link) => (
+            <div
+              className="snap-center shrink-0 pr-5"
+              key={link.id}
             >
-              {link.text}
-            </Link>
+              <Link
+                className="inline-flex items-center gap-x-2 hover:text-gray-500"
+                href={link.url}
+              >
+                {link.text}
+              </Link>
+            </div>
+          ))}
+        </div>
+        <div className="basis-1/2">
+          <div className="flex items-center p-4 text-sm text-blue-800 rounded-lg bg-blue-50" role="alert">
+            <Info className="mr-2"/>
+            <div>
+              <span className="font-medium">Info alert!</span> We have updated the app, contact us if you encounter problems.
+            </div>
           </div>
-        ))}
+        </div>
       </div>
     </nav>
   );
