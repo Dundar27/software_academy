22ef23bdfb4c894995b7d971177be95c04a80b48
Davut Burak Dündar <davutburakduundar@gmail.com>
2023-11-17 14:40:21 +0000

    Notification api created

--- src/app/api/notification/route.js
+++ src/app/api/notification/route.js
@@ -1,13 +1,22 @@
-import { NotificationAPI } from "@/app/api/notification/route";
-import ErrorAlert from "@/components/alerts/errorA";
+/* import ErrorAlert from "@/components/alerts/errorA";
 import InfoAlert from "@/components/alerts/infoA";
-import SuccessfulAlert from "@/components/alerts/successfulA";
+import SuccessfulAlert from "@/components/alerts/successfulA"; */
 
-export default async function NotificationP() {
+export async function NotificationAPI() {
+    const res = await fetch("https://software-academy-94090-default-rtdb.firebaseio.com/notifications.json");
 
+    if (!res.ok) {
+        // This will activate the closest `error.js` Error Boundary
+        throw new Error('Failed to fetch data')
+    }
+
+    return res.json()
+}
+
+/* export default async function getNotification() {
     const data = await NotificationAPI();
 
-    const getNotification = (condition, t, d, i, f, ft) => {
+    const component = (condition, t, d, i, f, ft) => {
 
         if (condition === "Successful") {
             return (<SuccessfulAlert title={t} description={d} isLink={i} forward={f} forwardText={ft} />)
@@ -23,9 +32,9 @@ export default async function NotificationP() {
         <div>
             {data.map((n, i) => (
                 <div key={i}>
-                    {getNotification(n.type, n.title, n.description, n.isLink, n.forward, n.forwardText)}
+                    {component(n.type, n.title, n.description, n.isLink, n.forward, n.forwardText)}
                 </div>
             ))}
         </div>
     )
-}
+} */
